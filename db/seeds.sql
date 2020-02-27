ff629ac3899b5f11f8d09f37e7e4d25348e3b50f
Gisele <giskey11@gmail.com>
2020-02-27 18:18:13 -0500

    Try example for the seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,23 +1,8 @@
-INSERT INTO `employee_db.departement` (`id`,`name`) VALUES (1,'Lab');
-INSERT INTO `employee_db.departement` (`id`,`name`) VALUES (2,'RH');
-INSERT INTO `employee_db.departement` (`id`,`name`) VALUES (3,'rr');
-INSERT INTO `employee_db.departement` (`id`,`name`) VALUES (4,'ss');
-INSERT INTO `employee_db.departement` (`id`,`name`) VALUES (5,'hhhhhhhhhhhhhhhh');
-INSERT INTO `employee_db.departement` (`id`,`name`) VALUES (6,'commu');
-INSERT INTO `employee_db.departement` (`id`,`name`) VALUES (7,'kkkkkkkkkkkkkkkkkkkkk');
-
-INSERT INTO `employee_db.employee` (`id`,`first_name`,`last_name`,`role_id`,`manager_id`) VALUES (1,'Ali','Koffi',1,2);
-INSERT INTO `employee_db.employee` (`id`,`first_name`,`last_name`,`role_id`,`manager_id`) VALUES (2,'John','Nina',2,2);
-INSERT INTO `employee_db.employee` (`id`,`first_name`,`last_name`,`role_id`,`manager_id`) VALUES (3,'Edey','Ane',2,2);
-INSERT INTO `employee_db.employee` (`id`,`first_name`,`last_name`,`role_id`,`manager_id`) VALUES (4,NULL,'wer',33,NULL);
-INSERT INTO `employee_db.employee` (`id`,`first_name`,`last_name`,`role_id`,`manager_id`) VALUES (5,NULL,'hhh',11,NULL);
-INSERT INTO `employee_db.employee` (`id`,`first_name`,`last_name`,`role_id`,`manager_id`) VALUES (6,'Gartien','Dam',2,5);
-
-INSERT INTO ` employee_db.role` (`id`,`title`,`salary`,`departement_id`) VALUES (1,'Admin',1000,1);
-INSERT INTO ` employee_db.role` (`id`,`title`,`salary`,`departement_id`) VALUES (2,'User',2000,2);
-INSERT INTO ` employee_db.role` (`id`,`title`,`salary`,`departement_id`) VALUES (3,'dddd',3333,6666);
-INSERT INTO ` employee_db.role` (`id`,`title`,`salary`,`departement_id`) VALUES (4,'uuu',888,999);
-INSERT INTO ` employee_db.role` (`id`,`title`,`salary`,`departement_id`) VALUES (5,'Intern',65,8);
-INSERT INTO ` employee_db.role` (`id`,`title`,`salary`,`departement_id`) VALUES (6,'Intern 2',65,9);
-INSERT INTO ` employee_db.role` (`id`,`title`,`salary`,`departement_id`) VALUES (7,'marketing',78888888,0);
+-- INSERT INTO `employee_db.departement` (`id`,`name`) VALUES (1,'Lab');
+-- INSERT INTO `employee_db.departement` (`id`,`name`) VALUES (2,'RH');
+-- INSERT INTO `employee_db.departement` (`id`,`name`) VALUES (3,'rr');
+-- INSERT INTO `employee_db.departement` (`id`,`name`) VALUES (4,'ss');
+-- INSERT INTO `employee_db.departement` (`id`,`name`) VALUES (5,'hhhhhhhhhhhhhhhh');
+-- INSERT INTO `employee_db.departement` (`id`,`name`) VALUES (6,'commu');
+-- INSERT INTO `employee_db.departement` (`id`,`name`) VALUES (7,'kkkkkkkkkkkkkkkkkkkkk');
 
